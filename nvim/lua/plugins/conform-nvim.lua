fa23c3ebe92c1f2fec2d9809fabe3ac9fb3b8ee0
James Lecher <jadlecher@gmail.com>
2024-02-04 09:38:35 -0600

    Automatically load confrom.nvim formatters with mason-tool-installer on conform initialize.

--- nvim/lua/plugins/conform-nvim.lua
+++ nvim/lua/plugins/conform-nvim.lua
@@ -1,27 +1,39 @@
 return {
-  "stevearc/conform.nvim",
-  dependencies = { 'williamboman/mason.nvim' },
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  keys = {
-    {
-      "<leader>f",
-      function()
-        require("conform").format({ async = true, lsp_fallback = true })
-      end,
-      mode = "",
-      desc = "Format buffer",
-    },
-  },
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      python = { "isort", "black" },
-      javascript = { { "prettierd" } },
-    },
-    format_on_save = { timeout_ms = 500, lsp_fallback = true },
-  },
-  init = function()
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
+	"stevearc/conform.nvim",
+	dependencies = { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			javascript = { { "prettierd" } },
+		},
+		format_on_save = { timeout_ms = 500, lsp_fallback = true },
+	},
+	init = function()
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
+	config = function(_, opts)
+		local conform = require("conform")
+		conform.setup(opts)
+		local required_formatters = vim.fn.map(conform.list_all_formatters(), function(_, formatter)
+			return formatter.name
+		end)
+		local installer = require("mason-tool-installer")
+		installer.setup({
+			ensure_installed = required_formatters,
+		})
+		vim.api.nvim_command("MasonToolsInstall")
+	end,
 }
