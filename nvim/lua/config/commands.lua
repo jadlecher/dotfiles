6d2a1c19a18941da455cc809b91fd5e9d970be54
James Lecher <jadlecher@gmail.com>
2024-08-31 06:39:31 -0500

    Reorganize mappings and commands and fix language server warnings

---
+++ nvim/lua/config/commands.lua
@@ -0,0 +1,15 @@
+vim.api.nvim_create_user_command("Scratch", function()
+	vim.cmd("enew")
+	vim.opt_local.buftype = "nofile"
+	vim.opt_local.bufhidden = "hide"
+	vim.opt_local.modified = false
+	vim.opt_local.swapfile = false
+end, {})
+
+---@param ctx table<string, any>
+vim.api.nvim_create_user_command("Redir", function(ctx)
+	local command = vim.api.nvim_parse_cmd(ctx.args, {})
+	local lines = vim.split(vim.api.nvim_cmd(command, { output = true }), "\n", { plain = true })
+	vim.cmd("Scratch")
+	vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+end, { nargs = "+", complete = "command" })
--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,7 +1,9 @@
 -- nvim-dap bindings
-local dap = require('dap')
-local dapui = require('dapui')
-vim.keymap.set("n", "<leader>bb", function() dap.repl.toggle({ height = 10 }) end)
+local dap = require("dap")
+local dapui = require("dapui")
+vim.keymap.set("n", "<leader>bb", function()
+	dap.repl.toggle({ height = 10 })
+end)
 vim.keymap.set("n", "<leader>bu", dapui.toggle)
 vim.keymap.set("n", "<leader>bp", dap.toggle_breakpoint)
 vim.keymap.set("n", "<leader>bx", dap.clear_breakpoints)
@@ -15,17 +17,13 @@ vim.keymap.set("n", "<leader>bh", dap.step_back)
 vim.keymap.set("n", "<leader>b.", dap.run_last)
 
 vim.keymap.set("n", "<leader>rr", function()
-  executable = require('utils.find-cmake-executable').find()
-  if executable ~= nil then vim.fn.system(executable) end
+	local executable = require("utils.find-cmake-executable").find()
+	if executable ~= nil then
+		vim.fn.system(executable)
+	end
 end)
 
 vim.keymap.set("n", "<leader>qf", "<cmd>lua vim.lsp.buf.code_action()<cr>")
+vim.keymap.set("n", "<leader>r", "<cmd>LspRestart<cr>")
 
 vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<cr>")
-
-vim.api.nvim_create_user_command('Redir', function(ctx)
-  local lines = vim.split(vim.api.nvim_exec(ctx.args, true), '\n', { plain = true })
-  vim.cmd('enew')
-  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
-  vim.opt_local.modified = false
-end, { nargs = '+', complete = 'command' })
