10e2f3ad70463f6bebb1bbdb750175e31ada11e7
James Lecher <jadlecher@gmail.com>
2024-12-01 20:49:03 -0600

    Add config directory

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+source_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 dry_run=false
 
 usage() {
@@ -28,37 +29,44 @@ handle_options() {
   done
 }
 install() {
-    echo "Creating symbolic link to $1 in $2."
-    if ! ${dry_run} ; then
-      ln -sf "$1" "$2"
+  echo "Creating symbolic link to $1 in $2."
+  if ! ${dry_run} ; then
+    ln -sf "$1" "$2"
+  fi
+}
+install_scripts() {
+  target=${HOME}/.local/bin
+  if [ ! -d "${target}" ]; then
+      echo "${fallback_target} does not exist, creating it."
+      mkdir "${target}"
+  fi
+  for script in $source_dir/scripts/*; do
+      if [ -f "$script" ]; then
+        source=$(readlink -f "${script}")
+        install "${source}" "${target}"
+      fi
+  done
+}
+install_configs() {
+  target=${XDG_CONFIG_DIR} # try the default config directory
+  fallback_target=~/.config # alternative dir to use if the default directory does not exist
+  if [ ! -d "${target}" ]; then
+    echo "XDG_CONFIG_DIR is not defined. Using ${fallback_target} instead."
+    target="${fallback_target}"
+    if [ ! -d "${target}" ]; then
+      echo "${fallback_target} does not exist, creating it."
+      mkdir "${target}"
     fi
+  fi
+  for entry in $source_dir/config/*/; do
+      if [ -d "$entry" ] ; then
+        source=$(readlink -f "${entry}")
+        install "${source}" "${target}"
+      fi
+  done
 }
 
 handle_options "$@"
 if ${dry_run} ; then echo "Executing dry run." ; fi
-
-target=${XDG_CONFIG_DIR} # try the default config directory
-fallback_target=~/.config # alternative dir to use if the default directory does not exist
-if [ ! -d "${target}" ]; then
-	echo "XDG_CONFIG_DIR is not defined. Using ${fallback_target} instead."
-	target="${fallback_target}"
-	if [ ! -d "${target}" ]; then
-		echo "${fallback_target} does not exist, creating it."
-		mkdir "${target}"
-	fi
-fi
-for entry in */; do
-    if [ -d "$entry" ] && [ "${entry}" != "scripts/" ]; then
-      source=$(readlink -f "${entry}")
-      install "${source}" "${target}"
-    fi
-done
-
-target=/usr/local/bin
-for script in scripts/*; do
-    if [ -f "$script" ]; then
-      source=$(readlink -f "${script}")
-      install "${source}" "${target}"
-    fi
-done
-echo "Done."
+install_scripts
+install_configs
