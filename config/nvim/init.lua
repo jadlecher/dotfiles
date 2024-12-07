38587986fd03d4e8c404a384e124a2d64862590c
James Lecher <jadlecher@gmail.com>
2024-12-06 19:40:38 -0600

    Add kitty-scrollback configuration

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,5 @@
 require("config.options")
 require("config.lazy")
+require("lazy").setup("plugins") -- load all plugins
 require("config.commands")
 require("config.mappings")
--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -1,16 +1,14 @@
 -- install lazy.nvim if not present
-local lazy_install_dir = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazy_install_dir = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazy_install_dir) then
-  print('Installing lazy.nvim....')
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazy_install_dir,
-  })
+	print("Installing lazy.nvim....")
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazy_install_dir,
+	})
 end
 vim.opt.rtp:prepend(lazy_install_dir) -- add lazy.nvim to rutime path
-
-require('lazy').setup("plugins")
