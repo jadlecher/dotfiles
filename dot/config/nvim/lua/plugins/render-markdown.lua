c2f5bf526bb300663fae8a3e6c94dbb847ceb796
James Lecher <jadlecher@gmail.com>
2025-05-25 08:51:20 -0500

    Remove broken configuration in favor of defaults

--- dot/config/nvim/lua/plugins/render-markdown.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	ft = function(_, filetypes)
-		-- Render markdown in codecompanion buffers
-		return vim.list_extend(filetypes, { "codecompanion" })
-	end,
-}
