f04cd941b4dca2114a8f990e0367fba7948c1aeb
James Lecher <jadlecher@gmail.com>
2025-08-08 08:27:34 -0500

    Disable LSP logging

--- dot/config/nvim/lua/config/options.lua
+++ dot/config/nvim/lua/config/options.lua
@@ -1,2 +1,5 @@
 vim.opt.clipboard = "" -- prevent LazyVim from overriding default clipbard
 -- vim.g.snacks_animate = false -- disable animations
+
+-- Disable LSP logs because they get very large!
+vim.lsp.set_log_level("off")
